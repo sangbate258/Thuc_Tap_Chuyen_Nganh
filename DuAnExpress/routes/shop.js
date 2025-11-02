8d255e2980057c746d3ef6c4f423bdd10dabeee9
sang <sangbate258@gmail.com>
2025-11-02 13:55:55 +0700

    Báo cáo tuần 2

--- DuAnExpress/routes/shop.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/shop', function(req, res, next) {
-    res.render('shop');
-});
-
-module.exports = router;
---
+++ WebsiteBanXeMay/app.js
@@ -0,0 +1,60 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var shopRouter = require('./routes/shop');
+var aboutRouter = require('./routes/about');
+var detailRouter = require('./routes/detail');
+var cartRouter = require('./routes/cart');
+var checkoutRouter = require('./routes/checkout');
+var blogRouter = require('./routes/blog');
+var blog_detailRouter = require('./routes/blog_detail');
+var teamRouter = require('./routes/team');
+var pricingRouter = require('./routes/pricing');
+var contactRouter = require('./routes/contact');
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/shop', shopRouter);
+app.use('/about', aboutRouter);
+app.use('/detail',detailRouter);
+app.use('/cart',cartRouter);
+app.use('/checkout',checkoutRouter);
+app.use('/blog',blogRouter);
+app.use('/blog_detail',blog_detailRouter);
+app.use('/team',teamRouter);
+app.use('/pricing',pricingRouter);
+app.use('/contact',contactRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
